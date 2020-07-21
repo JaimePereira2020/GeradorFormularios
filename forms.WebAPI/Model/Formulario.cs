b1823eccdcd37e9a241f41feb3c4b4d2351c7c67
Daniel Dias <ddias@prochild.local>
2020-07-21 16:11:08 +0100

    .

--- forms.WebAPI/Model/Formulario.cs
+++ forms.WebAPI/Model/Formulario.cs
@@ -7,9 +7,7 @@ namespace forms.WebAPI.Model
         public string description { get; set; }
         public string status { get; set; }
         public string version { get; set; }
-
         public string theme { get; set; }
-
         public int CreatorID { get; set; }
     }
 }
