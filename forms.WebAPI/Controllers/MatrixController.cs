e88372a9640d52c693740b7706584be462ff94dd
Daniel Dias <ddias@prochild.local>
2020-07-22 11:01:33 +0100

    metodos get getID

--- forms.WebAPI/Controllers/MatrixController.cs
+++ forms.WebAPI/Controllers/MatrixController.cs
@@ -1,7 +1,57 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using forms.WebAPI.Data;
+using forms.WebAPI.Model;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
 namespace forms.WebAPI.Controllers
 {
-    public class MatrixController
+     [Route("api/[controller]")]
+    [ApiController]
+    public class MatrixController : ControllerBase
     {
+         private readonly DataContext _context;
+
+        public MatrixController(DataContext context)
+        {
+            _context = context;
+        }
         
+        //GET ALL
+        [HttpGet]
+
+         public async Task<IActionResult> Get()
+        {
+            try
+            {
+                 var results = await _context.Matrix.ToListAsync();
+                 return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+                throw;
+            }
+        }
+        //GET by ID
+        [HttpGet("{id}")]
+
+       
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                 var results = await _context.Matrix.FirstOrDefaultAsync(x => x.MatrixID == id);
+                 return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+                throw;
+            }
+        }    
     }
 }
--- forms.WebAPI/Controllers/PossibilityAnswerController.cs
+++ forms.WebAPI/Controllers/PossibilityAnswerController.cs
@@ -2,6 +2,11 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using forms.WebAPI.Data;
 using forms.WebAPI.Model;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
 
 namespace forms.WebAPI.Controllers
 {  
@@ -10,17 +15,46 @@ namespace forms.WebAPI.Controllers
     [ApiController]
     public class PossibilityAnswerController : ControllerBase
     {
-     [HttpGet]
+    
+         private readonly DataContext _context;
 
-        public ActionResult<IEnumerable<PossibilityAnswer>> Get()
+        public PossibilityAnswerController(DataContext context)
         {
-            return new PossibilityAnswer[] {
-                new PossibilityAnswer (){
-                    PossibilityAnswerID=1,
-                    descriptionValuePossibilityAnswer = "Answer1"
-            
-                }
-             };
+            _context = context;
         }
+        
+        //GET ALL
+        [HttpGet]
+
+         public async Task<IActionResult> Get()
+        {
+            try
+            {
+                 var results = await _context.PossibilityAnswer.ToListAsync();
+                 return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+                throw;
+            }
+        }
+        //GET by ID
+        [HttpGet("{id}")]
+
+       
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                 var results = await _context.PossibilityAnswer.FirstOrDefaultAsync(x => x.PossibilityAnswerID == id);
+                 return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+                throw;
+            }
+        }    
     }
 }
