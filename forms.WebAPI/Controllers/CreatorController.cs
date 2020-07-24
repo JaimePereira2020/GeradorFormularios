c378c6f39bf5f2bb942ae42c7a03d406a0da2747
Jaime Pereira <jpereira@prochild.local>
2020-07-24 10:08:09 +0100

    Jaime API GETPOSTPUTDELETE_OK

--- forms.WebAPI/Controllers/CreatorController.cs
+++ forms.WebAPI/Controllers/CreatorController.cs
@@ -20,6 +20,8 @@ namespace forms.WebAPI.Controllers
         {
             _context = context;
         }
+
+      
         
         //GET ALL
         [HttpGet]
@@ -55,5 +57,91 @@ namespace forms.WebAPI.Controllers
                 throw;
             }
         }
+
+        // POST: api/user
+        [HttpPost]
+        public async Task<ActionResult<Creator>> PostCreator(Creator creator)
+        {
+            bool CreatorIDAlreadyExists = _context.Creator.Any(x => x.CreatorID == creator.CreatorID);
+            
+            if (ModelState.IsValid && CreatorIDAlreadyExists != true)
+            {
+                _context.Creator.Add(creator);
+                await _context.SaveChangesAsync();
+
+                //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+                return CreatedAtAction(nameof(Get), new { id = creator.CreatorID }, creator);
+            }
+           
+            else
+            {
+            return BadRequest("CreatorID already exists");
+
+            }
+            
+        }
+        
+        // PUT: api/User/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCreator(int id, Creator creator)
+        {
+            //bool UserExists(long id) =>_context.User.Any(e => e.UserID == id);
+            
+            if (id != creator.CreatorID)
+                {
+                    return BadRequest("The Creator ID does not exist");
+                }
+
+            _context.Entry(creator).State = EntityState.Modified;
+
+            try
+                {
+                    await _context.SaveChangesAsync();
+                }
+            catch (DbUpdateConcurrencyException)
+                {
+                    if (! CreatorExists(id))
+                    {
+                        return NotFound("User not found");
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+            return NoContent();
+        }
+
+        // DELETE: api/user/5
+        [HttpDelete("{id}")]
+         public async Task<ActionResult<Creator>> DeleteCreator(int id)
+        {
+            var creator = await _context.Creator.FindAsync(id);
+            if (creator == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Creator.Remove(creator);
+                await _context.SaveChangesAsync();
+                return (creator);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (! CreatorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        
+        private bool CreatorExists(int id) => _context.Creator.Any(e => e.CreatorID == id);    
     }
 }
